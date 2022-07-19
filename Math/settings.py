688f45f07c19f708721a3bc6cbba9e568f233edc
Ayim Nelson <ayimnelson01@gmail.com>
2022-07-19 00:04:31 +0000

    Updated Settings

--- Math/settings.py
+++ Math/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cef^u3j6kgb3d^%*m-%7srs8c1_te)s_og72p%rp#(m0bao59e'
+SECRET_KEY = '-cef^u3j6kgb3d^%*m-%7srs8c1_te)s_og72p%rp#(m0bao59e'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
